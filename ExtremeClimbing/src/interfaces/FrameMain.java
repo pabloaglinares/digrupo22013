b484c8dfb8ca94bf7e00aca2433e79bcae610ed5
USUARIO <USUARIO@10.0.31.1>
2014-01-21 13:51:47 +0100

    Primera subida

--- ExtremeClimbing/src/interfaces/FrameMain.java
+++ ExtremeClimbing/src/interfaces/FrameMain.java
@@ -1,7 +1,9 @@
 package interfaces;
 
 import java.awt.Component;
+import javax.swing.JFrame;
 import javax.swing.JInternalFrame;
+import javax.swing.UIManager;
 
 /**
  *
@@ -22,9 +24,17 @@ public class FrameMain extends javax.swing.JFrame {
     private void initComponents() {
 
         panel = new javax.swing.JDesktopPane();
-        jMenuBar1 = new javax.swing.JMenuBar();
-        jMenu1 = new javax.swing.JMenu();
-        jMenu2 = new javax.swing.JMenu();
+        barraMenu = new javax.swing.JMenuBar();
+        menuEntrenamiento = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        jMenuItem2 = new javax.swing.JMenuItem();
+        menuItinerarios = new javax.swing.JMenu();
+        jMenuItem3 = new javax.swing.JMenuItem();
+        jMenuItem4 = new javax.swing.JMenuItem();
+        menuResumenes = new javax.swing.JMenu();
+        menuPerfil = new javax.swing.JMenu();
+        menuAyuda = new javax.swing.JMenu();
+        jMenuItem5 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Extreme Climbing");
@@ -35,20 +45,58 @@ public class FrameMain extends javax.swing.JFrame {
         panel.setLayout(panelLayout);
         panelLayout.setHorizontalGroup(
             panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 637, Short.MAX_VALUE)
         );
         panelLayout.setVerticalGroup(
             panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 279, Short.MAX_VALUE)
+            .addGap(0, 394, Short.MAX_VALUE)
         );
 
-        jMenu1.setText("File");
-        jMenuBar1.add(jMenu1);
+        menuEntrenamiento.setText("Entrenamiento");
+        menuEntrenamiento.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
 
-        jMenu2.setText("Edit");
-        jMenuBar1.add(jMenu2);
+        jMenuItem1.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jMenuItem1.setText("Nueva sesión");
+        menuEntrenamiento.add(jMenuItem1);
 
-        setJMenuBar(jMenuBar1);
+        jMenuItem2.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jMenuItem2.setText("Consultar sesiones");
+        menuEntrenamiento.add(jMenuItem2);
+
+        barraMenu.add(menuEntrenamiento);
+
+        menuItinerarios.setText("Itinerarios");
+        menuItinerarios.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+
+        jMenuItem3.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jMenuItem3.setText("Nuevo itinerario");
+        menuItinerarios.add(jMenuItem3);
+
+        jMenuItem4.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jMenuItem4.setText("Consultar itinerarios");
+        menuItinerarios.add(jMenuItem4);
+
+        barraMenu.add(menuItinerarios);
+
+        menuResumenes.setText("Resúmenes");
+        menuResumenes.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        barraMenu.add(menuResumenes);
+
+        menuPerfil.setText("Perfil");
+        menuPerfil.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        barraMenu.add(menuPerfil);
+
+        menuAyuda.setText("?");
+        menuAyuda.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+
+        jMenuItem5.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_F1, 0));
+        jMenuItem5.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jMenuItem5.setText("Ayúdame");
+        menuAyuda.add(jMenuItem5);
+
+        barraMenu.add(menuAyuda);
+
+        setJMenuBar(barraMenu);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -81,38 +129,44 @@ public class FrameMain extends javax.swing.JFrame {
     }
 
     public static void main(String args[]) {
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+        /**
+         * Cambia el Look and Feel.
          */
         try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Metal".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FrameMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FrameMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FrameMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FrameMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (Exception e) {
+            System.err.println("Surgió un error cambiando el Look&Feel");
         }
-        //</editor-fold>
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new FrameMain().setVisible(true);
+                FrameMain principal = new FrameMain();
+                /**
+                 * Inicia la aplicación Maximizada.
+                 */
+                principal.setExtendedState(JFrame.MAXIMIZED_BOTH);
+                principal.setVisible(true);
             }
         });
+        
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JMenu jMenu1;
-    private javax.swing.JMenu jMenu2;
-    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuBar barraMenu;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
+    private javax.swing.JMenuItem jMenuItem4;
+    private javax.swing.JMenuItem jMenuItem5;
+    private javax.swing.JMenu menuAyuda;
+    private javax.swing.JMenu menuEntrenamiento;
+    private javax.swing.JMenu menuItinerarios;
+    private javax.swing.JMenu menuPerfil;
+    private javax.swing.JMenu menuResumenes;
     private javax.swing.JDesktopPane panel;
     // End of variables declaration//GEN-END:variables
 }
