8d28451d4dc1247a77be36741cb06cb3fd6e80f9
leroysvaldes <leroys.aglinares@gmail.com>
2014-01-22 09:04:15 +0100

    Primera subida

---
+++ ExtremeClimbing/help/help_set.hs
@@ -0,0 +1,27 @@
+<helpset version="1.0">
+ <title>Practica AJEDREZ</title>
+ <maps>
+ <homeID>indice</homeID>
+ <mapref location="map_file.jhm"/>
+ </maps>
+ <view>
+ <name>Tabla Contenidos</name>
+ <label>Tabla de contenidos</label>
+ <type>javax.help.TOCView</type> 
+<data>toc.xml</data>
+ </view>
+  <view>
+<name>Indice</name>
+ <label>El indice</label>
+ <type>javax.help.IndexView</type>
+ <data>index.xml</data>
+ </view>
+ <view>
+ <name>Buscar</name>
+ <label>Buscar</label>
+ <type>javax.help.SearchView</type>
+ <data engine="com.sun.java.help.search.DefaultSearchEngine">
+JavaHelpSearch 
+</data>
+ </view>
+</helpset>
--- ExtremeClimbing/src/interfaces/FrameMain.java
+++ ExtremeClimbing/src/interfaces/FrameMain.java
@@ -1,6 +1,11 @@
 package interfaces;
 
+import interfaces.sesiones.IFrameSesiones;
 import java.awt.Component;
+import java.io.File;
+import java.net.URL;
+import javax.help.HelpBroker;
+import javax.help.HelpSet;
 import javax.swing.JFrame;
 import javax.swing.JInternalFrame;
 import javax.swing.UIManager;
@@ -15,6 +20,7 @@ public class FrameMain extends javax.swing.JFrame {
     
     public FrameMain() {
         initComponents();
+        iniciarAyuda();//revisar
         setLocationRelativeTo(null);
     }
 
@@ -25,19 +31,19 @@ public class FrameMain extends javax.swing.JFrame {
 
         panel = new javax.swing.JDesktopPane();
         jToolBar2 = new javax.swing.JToolBar();
-        panelRendimiento = new javax.swing.JPanel();
-        jLabel2 = new javax.swing.JLabel();
         barraMenu = new javax.swing.JMenuBar();
         menuEntrenamiento = new javax.swing.JMenu();
-        jMenuItem1 = new javax.swing.JMenuItem();
-        jMenuItem2 = new javax.swing.JMenuItem();
+        menuNuevoEntrenamiento = new javax.swing.JMenuItem();
+        menuConsultaEntrenamientos = new javax.swing.JMenuItem();
         menuItinerarios = new javax.swing.JMenu();
-        jMenuItem3 = new javax.swing.JMenuItem();
-        jMenuItem4 = new javax.swing.JMenuItem();
+        menuNuevoItinerario = new javax.swing.JMenuItem();
+        menuConsultaItinerarios = new javax.swing.JMenuItem();
         menuResumenes = new javax.swing.JMenu();
+        menuResumen = new javax.swing.JMenuItem();
         menuPerfil = new javax.swing.JMenu();
+        menuEscalador = new javax.swing.JMenuItem();
         menuAyuda = new javax.swing.JMenu();
-        jMenuItem5 = new javax.swing.JMenuItem();
+        ayuda = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Extreme Climbing");
@@ -48,90 +54,75 @@ public class FrameMain extends javax.swing.JFrame {
         jToolBar2.setFloatable(false);
         jToolBar2.setRollover(true);
 
-        panelRendimiento.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Tu rendimiento ", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Tahoma", 1, 14), new java.awt.Color(0, 51, 153))); // NOI18N
-
-        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 90)); // NOI18N
-        jLabel2.setText("0");
-
-        javax.swing.GroupLayout panelRendimientoLayout = new javax.swing.GroupLayout(panelRendimiento);
-        panelRendimiento.setLayout(panelRendimientoLayout);
-        panelRendimientoLayout.setHorizontalGroup(
-            panelRendimientoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(panelRendimientoLayout.createSequentialGroup()
-                .addGap(32, 32, 32)
-                .addComponent(jLabel2)
-                .addContainerGap(35, Short.MAX_VALUE))
-        );
-        panelRendimientoLayout.setVerticalGroup(
-            panelRendimientoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(panelRendimientoLayout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel2)
-                .addContainerGap(24, Short.MAX_VALUE))
-        );
-
         javax.swing.GroupLayout panelLayout = new javax.swing.GroupLayout(panel);
         panel.setLayout(panelLayout);
         panelLayout.setHorizontalGroup(
             panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jToolBar2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelLayout.createSequentialGroup()
-                .addContainerGap(473, Short.MAX_VALUE)
-                .addComponent(panelRendimiento, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+            .addComponent(jToolBar2, javax.swing.GroupLayout.DEFAULT_SIZE, 620, Short.MAX_VALUE)
         );
         panelLayout.setVerticalGroup(
             panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelLayout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(panelRendimiento, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 173, Short.MAX_VALUE)
+                .addContainerGap(354, Short.MAX_VALUE)
                 .addComponent(jToolBar2, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         panel.setLayer(jToolBar2, javax.swing.JLayeredPane.DEFAULT_LAYER);
-        panel.setLayer(panelRendimiento, javax.swing.JLayeredPane.DEFAULT_LAYER);
 
         menuEntrenamiento.setText("Entrenamiento");
         menuEntrenamiento.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
 
-        jMenuItem1.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        jMenuItem1.setText("Nueva sesión");
-        menuEntrenamiento.add(jMenuItem1);
+        menuNuevoEntrenamiento.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        menuNuevoEntrenamiento.setText("Nueva sesión");
+        menuEntrenamiento.add(menuNuevoEntrenamiento);
 
-        jMenuItem2.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        jMenuItem2.setText("Consultar sesiones");
-        menuEntrenamiento.add(jMenuItem2);
+        menuConsultaEntrenamientos.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        menuConsultaEntrenamientos.setText("Consultar sesiones");
+        menuConsultaEntrenamientos.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                menuConsultaEntrenamientosActionPerformed(evt);
+            }
+        });
+        menuEntrenamiento.add(menuConsultaEntrenamientos);
 
         barraMenu.add(menuEntrenamiento);
 
         menuItinerarios.setText("Itinerarios");
         menuItinerarios.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
 
-        jMenuItem3.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        jMenuItem3.setText("Nuevo itinerario");
-        menuItinerarios.add(jMenuItem3);
+        menuNuevoItinerario.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        menuNuevoItinerario.setText("Nuevo itinerario");
+        menuItinerarios.add(menuNuevoItinerario);
 
-        jMenuItem4.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        jMenuItem4.setText("Consultar itinerarios");
-        menuItinerarios.add(jMenuItem4);
+        menuConsultaItinerarios.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        menuConsultaItinerarios.setText("Consultar itinerarios");
+        menuItinerarios.add(menuConsultaItinerarios);
 
         barraMenu.add(menuItinerarios);
 
         menuResumenes.setText("Resúmenes");
         menuResumenes.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+
+        menuResumen.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        menuResumen.setText("Rendimiento");
+        menuResumenes.add(menuResumen);
+
         barraMenu.add(menuResumenes);
 
         menuPerfil.setText("Perfil");
         menuPerfil.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+
+        menuEscalador.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        menuEscalador.setText("Escalador");
+        menuPerfil.add(menuEscalador);
+
         barraMenu.add(menuPerfil);
 
         menuAyuda.setText("?");
         menuAyuda.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
 
-        jMenuItem5.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_F1, 0));
-        jMenuItem5.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        jMenuItem5.setText("Ayúdame");
-        menuAyuda.add(jMenuItem5);
+        ayuda.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        ayuda.setText("Ayúdame");
+        menuAyuda.add(ayuda);
 
         barraMenu.add(menuAyuda);
 
@@ -151,6 +142,33 @@ public class FrameMain extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void menuConsultaEntrenamientosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuConsultaEntrenamientosActionPerformed
+        cerrarDialogosAbiertos();
+        IFrameSesiones iFrameSesiones = new IFrameSesiones();
+        
+    }//GEN-LAST:event_menuConsultaEntrenamientosActionPerformed
+
+    /**
+     * Método para iniciar la ayuda.
+     */
+    private void iniciarAyuda(){
+        try {
+            // Carga el fichero de ayuda
+            File fichero = new File("help" + File.separator + "help_set.hs");
+            URL hsURL = fichero.toURI().toURL();
+            // Crea el HelpSet y el HelpBroker
+            HelpSet helpset = new HelpSet(getClass().getClassLoader(), hsURL);
+            HelpBroker hb = helpset.createHelpBroker();
+            // Pone ayuda a item de menu al pulsarlo y a F1 en ventana
+            // principal y secundaria.
+            hb.enableHelpKey(getRootPane(), "indice", helpset);
+            hb.enableHelpOnButton(ayuda, "indice", helpset);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    
     /**
      * Cierra los dialogos abiertos antes de abrir otro que sea IFrame.
      */
@@ -195,20 +213,20 @@ public class FrameMain extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenuItem ayuda;
     private javax.swing.JMenuBar barraMenu;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JMenuItem jMenuItem1;
-    private javax.swing.JMenuItem jMenuItem2;
-    private javax.swing.JMenuItem jMenuItem3;
-    private javax.swing.JMenuItem jMenuItem4;
-    private javax.swing.JMenuItem jMenuItem5;
     private javax.swing.JToolBar jToolBar2;
     private javax.swing.JMenu menuAyuda;
+    private javax.swing.JMenuItem menuConsultaEntrenamientos;
+    private javax.swing.JMenuItem menuConsultaItinerarios;
     private javax.swing.JMenu menuEntrenamiento;
+    private javax.swing.JMenuItem menuEscalador;
     private javax.swing.JMenu menuItinerarios;
+    private javax.swing.JMenuItem menuNuevoEntrenamiento;
+    private javax.swing.JMenuItem menuNuevoItinerario;
     private javax.swing.JMenu menuPerfil;
+    private javax.swing.JMenuItem menuResumen;
     private javax.swing.JMenu menuResumenes;
     private javax.swing.JDesktopPane panel;
-    private javax.swing.JPanel panelRendimiento;
     // End of variables declaration//GEN-END:variables
 }
